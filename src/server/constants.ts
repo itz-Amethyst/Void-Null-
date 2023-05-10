92558842e602d35c94d57afbd56410aece1ed60c
callme-devil <pransermi@gmail.com>
2023-05-11 01:50:47 +0430

    fix

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -6,6 +6,7 @@ function env(key: string) {
 	return value;
 }
 
+export const Github_UserName = env('Github_UserName')
 export const DISCORD_WEBHOOK = env('DISCORD_WEBHOOK');
 export const LAST_FM_API_KEY = env('LAST_FM_API_KEY');
 export const SPOTIFY_CLIENT_ID = env('SPOTIFY_CLIENT_ID');
