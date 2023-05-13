9853a054cd85417370949629f6b2a0f7a8602f41
callme-devil <pransermi@gmail.com>
2023-05-13 21:21:50 +0430

    cleanup

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -9,11 +9,11 @@ function env(key: string) {
 export const DISCORD_ID = env('DISCORD_ID');
 export const Github_UserName = env('Github_UserName')
 export const DISCORD_WEBHOOK = env('DISCORD_WEBHOOK');
-export const LAST_FM_API_KEY = env('LAST_FM_API_KEY');
+// export const LAST_FM_API_KEY = env('LAST_FM_API_KEY');
 export const SPOTIFY_CLIENT_ID = env('SPOTIFY_CLIENT_ID');
 export const SPOTIFY_CLIENT_SECRET = env('SPOTIFY_CLIENT_SECRET');
-export const GRAPHQL_URL = env('NEXT_PUBLIC_GRAPHQL_URL');
-export const SPOTIFY_REDIRECT_URI = 'http://localhost:3000/api/spotify/oauth';
+// export const GRAPHQL_URL = env('NEXT_PUBLIC_GRAPHQL_URL');
+// export const SPOTIFY_REDIRECT_URI = 'http://localhost:3000/api/spotify/oauth';
 
 export const REDIS_URL = env('REDIS_URL');
 
