8115df353d4fb814cae04a31c883a594eafd125a
itz-amethyst <pransermi@gmail.com>
2023-06-01 11:26:14 +0430

    fix: fixed daysUntilBirthday bug

--- src/util/time.ts
+++ src/util/time.ts
@@ -25,13 +25,17 @@ export const RelativeTimeFormatter = new Intl.RelativeTimeFormat('en', {
 
 export const dob = new Date('2004-04-04');
 export const age = new Date(Date.now() - dob.getTime()).getUTCFullYear() - 1970;
-export const hasHadBirthdayThisYear = new Date().getMonth() >= dob.getMonth() && new Date().getDate() >= dob.getDate();
-export const nextBirthdayYear = new Date().getFullYear() + (hasHadBirthdayThisYear ? 1 : 0);
+const today = new Date();
+
+export const hasHadBirthdayThisYear = (
+	today.getMonth() > dob.getMonth() || (today.getMonth() === dob.getMonth() && today.getDate() >= dob.getDate())
+) && today.getFullYear() >= dob.getFullYear();
+export const nextBirthdayYear = today.getFullYear() + (hasHadBirthdayThisYear ? 1 : 0);
 export const daysUntilBirthday = RelativeTimeFormatter.formatToParts(
     // ? Fix
 
 	Math.floor(
-		(new Date(nextBirthdayYear, dob.getMonth(), dob.getDay() + 1).getTime() - Date.now()) / 1000 / 60 / 60 / 24,
+		(new Date(nextBirthdayYear, dob.getMonth(), dob.getDay() + 1).getTime() - today.getTime()) / 1000 / 60 / 60 / 24,
 	),
 	'day',
 )[1]!.value.toString();
